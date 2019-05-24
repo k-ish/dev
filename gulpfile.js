5dc960800b7dcb8680d490ced06fdb5ba4edf9f2
石崎和彦 <ishizaki@ishizaki.local>
2019-05-24 10:43:38 +0900

    gulp renew

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,120 @@
-const path = require("path");   // パス取得（標準モジュール）
-const fs = require("fs");   // ファイル操作（標準モジュール）
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");	// postcssの一部
-const mqpacker = require("css-mqpacker");	// postcssの一部
-const pug = require("gulp-pug");
-const plumber = require("gulp-plumber");    // errorでも監視継続
-const rename = require("gulp-rename");
+const gulp = require('gulp'),
+			sass = require('gulp-sass'),
+			postcss = require('gulp-postcss'),
+			autoprefixer = require('autoprefixer'),	// postcssの一部
+			mqpacker = require('css-mqpacker'),	// postcssの一部
+			cssdeclsort = require('css-declaration-sorter'),	// postcssの一部(並び替え)
+			pug = require('gulp-pug'),
+			plumber = require("gulp-plumber"),	// 監視継続
+			notify  = require('gulp-notify'),	// 通知
+			imagemin = require("gulp-imagemin"),
+			imageminJpg = require("imagemin-mozjpeg"),
+			imageminPng = require("imagemin-pngquant"),
+			imageminGif = require("imagemin-gifsicle");
 
-// https://qiita.com/MikaShirahama/items/ab91624709510c496e53
-const imagemin = require("gulp-imagemin");
-const imageminJpg = require("imagemin-mozjpeg");
-const imageminPng = require("imagemin-pngquant");
-const imageminGif = require("imagemin-gifsicle");
-const changed = require('gulp-changed');
 
-// npm i -D gulp@3.9.0 gulp-sass gulp-postcss css-mqpacker autoprefixer gulp-pug gulp-plumber gulp-rename gulp-imagemin imagemin-mozjpeg imagemin-pngquant imagemin-gifsicle gulp-changed
-// gulp.task(“タスク名”,function() {});でタスクの登録をおこないます。
-// gulp.src(“MiniMatchパターン”)で読み出したいファイルを指定します。
+
+// path 設定
+var rootdir = './dest';
+var destdir = rootdir;
+var srcdir = {
+	scss: './src/**/*.scss',
+	pug: ['./src/**/*.pug', '!src/**/_*.pug', '!./src/_*/**/*'],
+	image: './src/**/*.+(jpg|jpeg|png|gif)',
+	copy: ['./src/**/*','!./src/**/*.+(pug|scss|jpg|jpeg|png|gif)']
+}
+
+
+
+// sass コンパイル
+gulp.task('sass', function () {
+	// post-css プラグインの設定
+	var plugins = [
+		cssdeclsort({
+			// smacss 沿ってソート
+			order:'smacss'
+		}),
+		autoprefixer({
+			// IEは11以上、Androidは4.4以上
+			// その他は最新2バージョンで必要なベンダープレフィックスを付与する設定
+			browsers: ['last 2 versions', 'ie >= 10', 'Android >= 4'],
+			cascade: false
+		}),
+		// media-query をまとめる
+		mqpacker()
+	];
+
+	return gulp.src(srcdir.scss)
+	// return gulp.src(srcdir.scss, { sourcemaps: true })
+		.pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))	// watch　エラー時間師継続+DP通知
+		.pipe(sass({ outputStyle: 'expanded' }))
+		.pipe(postcss(plugins))	// 設定したpost-cssを実行
+		.pipe(gulp.dest(destdir))
+		// .pipe(gulp.dest(destdir,{ sourcemaps: './maps' }))
+});
+
+
+
+// pug コンパイル
+gulp.task('pug', function () {
+	return gulp.src(srcdir.pug)
+		.pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))	// watch　エラー時間師継続+DP通知
+		.pipe(pug({ pretty: true }))
+		.pipe(gulp.dest(destdir))
+});
+
+
+
+// 画像圧縮
+gulp.task('imagemin', function () {
+	return gulp.src(srcdir.image, {	// 差分更新ができない
+		since: gulp.lastRun(imagemin)
+	})
+	.pipe(imagemin([
+		imageminPng(),
+		imageminJpg({
+			quality: 85,
+			progressive: true
+		}),
+		imageminGif({
+			interlaced: false,
+			optimizationLevel: 3,
+			colors: 180
+		})
+	]
+	))
+	.pipe(gulp.dest(destdir))
+});
+
+
+
+// ファイルコピー
+gulp.task('copy', function () {
+	return gulp.src(srcdir.copy)
+		.pipe(gulp.dest(destdir));
+});
+
+
+
+// 監視
+gulp.task('default', function () {
+	gulp.watch(srcdir.scss, gulp.task('sass'));
+	gulp.watch(srcdir.pug, gulp.task('pug'));
+	gulp.watch(srcdir.image, gulp.task('imagemin'));
+	gulp.watch(srcdir.copy, gulp.task('copy'));
+});
+
+
+
+//　ビルド
+gulp.task('build', gulp.series(gulp.parallel('sass', 'pug', 'copy', 'imagemin')));
+
+
+
+// gulp-sass gulp-postcss autoprefixer css-mqpacker gulp-pug gulp-plumber gulp-notify
+
+// gulp.task(“タスク名”,function() {  anytask  });でタスクの登録をおこないます。
+// return gulp.src(“MiniMatchパターン”)で読み出したいファイルを指定します。
 // pipe(行いたい処理)でsrcで取得したファイルに処理を施します
 // gulp.dest(“出力先”)で出力先に処理を施したファイルを出力します。
 // “sass/style.scss” sass/style.scssだけヒット
@@ -26,112 +122,12 @@ const changed = require('gulp-changed');
 // “sass/**/*.scss” sassディレクトリ以下にあるすべてのscssがヒット
 // [“sass/**/.scss”,"!sass/sample/**/*.scss] sass/sample以下にあるscssを除くsassディレクトリ以下のscssがヒット
 
-
-var basedir = "DIST/";   // ルートディレクトリを指定
-var dir = basedir;
-
-// jpg,png,gif画像の圧縮タスク
-var paths = {
-	srcDir: 'src',
-	dstDir: dir
-}
-gulp.task('imagemin', function () {
-	var srcGlob = paths.srcDir + '/**/*.+(jpg|jpeg|png|gif)';
-	var dstGlob = paths.dstDir;
-	gulp.src(srcGlob)
-		.pipe(changed(dstGlob))
-		.pipe(imagemin([
-			imageminPng(),
-			imageminJpg({
-				quality: 80
-
-			}),
-			imageminGif({
-				interlaced: false,
-				optimizationLevel: 3,
-				colors: 180
-			})
-		]
-		))
-		.pipe(gulp.dest(dstGlob));
-});
-
-
-// sassコンパイル
-gulp.task("sass", function () {
-	// post-css プラグインの設定
-	var plugins = [
-		autoprefixer({
-			// ☆IEは11以上、Androidは4.4以上
-			// その他は最新2バージョンで必要なベンダープレフィックスを付与する設定
-			browsers: ["last 2 versions", "ie >= 10", "Android >= 4"],
-			cascade: false
-		}),
-		// media-query をまとめる
-		mqpacker()
-	];
-
-	gulp.src(["./src/**/*.scss", "!./src/_*/**/*"])
-		.pipe(sass({ outputStyle: "expanded" })) // sassコンパイル
-		.on("error", sass.logError) // 監視中のエラーによる強制停止を回避
-		.pipe(postcss(plugins))	// 設定したpost-cssを実行
-		.pipe(gulp.dest(dir));
-});
-
-
-// pug コンパイル
-gulp.task("pug", function () {
-	gulp.src(["./src/**/*.pug", "!src/**/_*.pug", "!./src/_*/**/*"])
-		.pipe(plumber()) // 監視中のエラーによる強制停止を回避
-		.pipe(
-			pug({
-				pretty: true
-			})
-		)
-		.pipe(gulp.dest(dir));
-});
-
-
-// コピー
-gulp.task("copy", function () {
-	gulp.src(["./src/**/*.+(html|css|js|pdf)", "!./src/_*/**/*"])
-		.pipe(gulp.dest(dir));
-});
-
-
-
-
-// 監視
-gulp.task("default", function () {
-	gulp.watch(["./src/**/*.pug", "!src/**/_*.pug", "!./src/_*/**/*"], ["pug"]);
-	gulp.watch(["./src/**/*.scss", "!./src/_*/**/*"], ["sass"]);
-	gulp.watch(["./src/**/*.+(html|css|js|pdf)", "!./src/_*/**/*"], ["copy"]);
-	gulp.watch("srcGlob", ["imagemin"]);
-});
-
-
-// 初回実行
-gulp.task("build", ['sass', 'pug', 'copy', 'imagemin']);
-
-
-
-
-// ファイルコピー
-// ゼロパディング関数
-function zeroPadding(num, length) {
-	return ('0000000000' + num).slice(-length);
-}
-gulp.task("copy2", function () {
-	for (i = 1; i <= 20; i++) {     // 条件に生成する回数
-		var copyNum = zeroPadding(i, 2);    // ゼロパディングの右値が桁、左値が実際の数
-		gulp.src("index.html")      // コピー元
-			.pipe(rename({
-				dirname: "test2",   // ディレクトリ構造
-				basename: "index",  // 基本名
-				prefix: "a-",       // 先頭文字
-				suffix: copyNum,    // 後尾文字
-				extname: ".css"     // 拡張子
-			}))
-			.pipe(gulp.dest('./dist'));
-	}
-});
+// では新機能を確認します。
+// 1. タスクを直列処理するseries()と並列処理するparallel()メソッドが追加。run-sequenceプラグインはもう必要ない
+// 2. watch()タスクはchokidarを使用したものに変更。gulp-watchプラグインはもう必要ない
+// 3. lastRun()を使用して差分ビルドが簡単に。gulp-changedプラグインはもう必要ない
+// 4. symlink()でシンボリックリンクを作成しpackage.jsonとnode_modulesを使いまわすことが可能に
+// 5. ソースマップ組み込みサポートが追加。gulp-sourcemapsプラグインはもう必要ない
+// 6. エクスポートされたfunctionのtask登録
+// 7. カスタム・レジストリ（タスクの分割）がもっと簡単にできるようになった
+// 8. 条件付きビルドと段階的ビルドがいろいろとアップデートされた
